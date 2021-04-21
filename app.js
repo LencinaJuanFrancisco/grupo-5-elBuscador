fbb3a2ac9aa41841f9bf99a02a4568556118112b
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2021-04-21 07:17:16 -0300

    se crean los archivos iniciales y las rutas de axceso en app.js, todas las rutas funcionando

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+
+const app= express();
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname,'./public')))
+
+
+app.listen(3000,()=>{
+    console.log('SERVER corriendo en http://localhost:3000/index')
+})
+
+app.get('/index',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/index.html'))
+})
+app.get('/carrito',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/carrito.html'))
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
+app.get('/producto',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/producto.html'))
+})
+app.get('/registro',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/registro.html'))
+})
