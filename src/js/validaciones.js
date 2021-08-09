8361af5ad961f7b94697c19b11114d79bc6431be
LencinaJuanFrancisco <juanfranciscolencina@gmail.com>
2021-08-09 18:17:55 -0300

    se agregan algunas correcciones en validaciones.js

--- src/js/validaciones.js
+++ src/js/validaciones.js
@@ -1,51 +1,45 @@
-window.addEventListener('load', function(){
+window.addEventListener("load", function () {
+  let form = document.querySelector("form.formulario");
 
-let form = document.querySelector('form');
-
-
-form.addEventListener('submit', (e)=>{
-
+  form.addEventListener("submit", (e) => {
     const errors = [];
 
+    let nombre = document.querySelector("input.fullName");
 
-    let nombre = document.querySelector('input.fullName');
-
-    if(nombre.value ==''|| nombre.value < 2){
-        errors.push('El nombre debe tener al menos 2 caracteres');
+    if (nombre.value == "") {
+      errors.push("El nombre debe tener al menos 2 caracteres");
+    } else if (nombre.value.length < 5) {
+      errors.push("El nombre debe contener mas de 5 caracteres");
     }
 
-    let avatar = document.querySelector('input.avatar');
+    let avatar = document.querySelector("input.avatar");
 
-    if(avatar.value ==''){
-        errors.push('Debe seleccionar una imagen en su avatar');
+    if (avatar.value == "") {
+      errors.push("Debe seleccionar una imagen en su avatar");
     }
 
-    let email = document.querySelector('input.email'); 
-    
-    if(email.value ==''){
-        errors.push('Debes ingresar un mail valido');
+    let email = document.querySelector("input.email");
+
+    if (email.value == "") {
+      errors.push("Debes ingresar un mail valido");
     }
 
-    let contraseña = document.querySelector('input.password');
+    let contraseña = document.querySelector("input.password");
 
-    if(contraseña.value == ''){
-        errors.push('Debes ingresar una contraseña')
+    if (contraseña.value == "") {
+      errors.push("Debes ingresar una contraseña");
     }
+    console.log("errores de validacion FRONT---------------------");
+    console.log("--------" + errors);
+    console.log("----------------------------------------****----");
+    if (errors.length > 0) {
+      e.preventDefault();
 
-if (errors.length>0){
-    e.preventDefault();
-    
-    let ulErrores = document.getElementById('.errores');
-    
-    for (let i= 0; i<errors.length; i++){
+      let ulErrores = document.querySelector("div.errores ul");
 
-        
-    ulErrores.innerHTML += "<li>"+errors[i]+"</li>"
+      for (let i = 0; i < errors.length; i++) {
+        ulErrores.innerHTML += "<li>" + errors[i] + "</li>";
+      }
     }
-    
-
-}
-
-
-})
-})
+  });
+});
