70d2ac08107433f90ad2174e7ead98dfe7e280cf
Jorge Gorostegui Di Peto <jorge.gdp@gmail.com>
2021-08-23 19:29:08 -0300

    apiProducts terminada

--- src/routers/api/producto.js
+++ src/routers/api/producto.js
@@ -1,28 +1,9 @@
 const express= require('express');
 const router = express.Router();
-const db = require('../../database/models')
+const controllerProducto = require("../../controllers/apiControlles/apiProducts")
 
-router.get('/producto',(req,res)=>{
-    db.Producto.findAll()
-    .then(productos => res.json({
-        total: productos.length,
-        data: productos,
-        status: 200
-    }))
-
-})
-
-// FALTA DESDE ACA - EL FILTRADO POR CATEGORIAS 
-router.get('/producto/temporada',(req,res)=>{
-    db.Producto.findAll({
-        where: {
-            temporada_id: 2,
-        }
-    })
-    .then(productos => {
-        return res.status(200).json(produtos);
-    })
-})
+router.get('/', controllerProducto.listadoProducto)
+router.get('/:id', controllerProducto.detalleProducto)
 
 
-module.exports= router;
+module.exports = router;
