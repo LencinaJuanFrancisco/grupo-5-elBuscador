b90a3b090bf8501900358c497e036934c2dd2980
Jorge Gorostegui Di Peto <jorge.gdp@gmail.com>
2021-08-30 20:50:41 -0300

    Protección de rutas con middlewares

--- src/routers/productos.js
+++ src/routers/productos.js
@@ -2,24 +2,26 @@ const express= require('express');
 const router = express.Router();
 const controllerProducto= require('../controllers/controllerProductos');
 const uploadFile = require("../middelwares/multermiddelware")
+const authMiddleware = require("../middelwares/authMiddelware");
 
 //router.get('/', controllerProducto.detalle);
 router.get('/listadoProducto', controllerProducto.listadoProducto)
-router.get('/carga', controllerProducto.viewCarga)
+router.get('/carga', authMiddleware, controllerProducto.viewCarga)
 router.post('/carga', uploadFile.single('imgPrincipal')
 , controllerProducto.store)
 
 
 
-router.get('/editar/:id', controllerProducto.viewEdit)
+
+router.get('/editar/:id', authMiddleware, controllerProducto.viewEdit)
 router.put('/editar/:id', uploadFile.single('imgPrincipal')
 , controllerProducto.update)
 
-router.delete('/borrar/:id', controllerProducto.delete)
+router.delete('/borrar/:id', authMiddleware, controllerProducto.delete)
 
 
 router.get('/carrito', controllerProducto.carrito);
 
 router.get('/:id', controllerProducto.detalle);
 
-module.exports = router;                        
+module.exports = router;
